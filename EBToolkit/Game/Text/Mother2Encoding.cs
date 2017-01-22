eb72f454761a7c1c2e179eab2f33a3f4f88003f5
justink_ase <justinkrejcha393@gmail.com>
2017-01-22 14:27:18 -0800

    Make it compileable.

--- EBToolkit/Game/Text/Mother2Encoding.cs
+++ EBToolkit/Game/Text/Mother2Encoding.cs
@@ -11,5 +11,44 @@ namespace EBToolkit.Game.Text
 	/// </summary>
 	public class Mother2Encoding : Encoding
 	{
+		public override int GetByteCount(char[] chars, int index, int count)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override int GetBytes(char[] chars, int charIndex, int charCount, byte[] bytes, int byteIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override int GetCharCount(byte[] bytes, int index, int count)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override int GetChars(byte[] bytes, int byteIndex, int byteCount, char[] chars, int charIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// Return the max byte count for this encoding
+		/// </summary>
+		/// <param name="charCount">The amount of characters</param>
+		/// <returns><paramref name="charCount"/></returns>
+		public override int GetMaxByteCount(int charCount)
+		{
+			return charCount;
+		}
+
+		/// <summary>
+		/// Return the max character count for this encoding
+		/// </summary>
+		/// <param name="byteCount">The amount of bytes encoded</param>
+		/// <returns><paramref name="byteCount"/></returns>
+		public override int GetMaxCharCount(int byteCount)
+		{
+			return byteCount;
+		}
 	}
 }
